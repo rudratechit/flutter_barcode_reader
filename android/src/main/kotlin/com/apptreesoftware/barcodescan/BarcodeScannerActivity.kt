96d011a5d7a4f0fb069bcec55519b9386c7e023e
Sumit Chouhan <sumit1.chouhan@ril.com>
2021-10-21 03:42:21 +0530

    Code update

--- android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
+++ android/src/main/kotlin/com/apptreesoftware/barcodescan/BarcodeScannerActivity.kt
@@ -27,35 +27,34 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
         title = ""
         scannerView = ZXingScannerView(this)
         scannerView.setAutoFocus(true)
-        scannerView.flash = true
         // this paramter will make your HUAWEI phone works great!
         scannerView.setAspectTolerance(0.5f)
         setContentView(scannerView)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        if (flag) {
+//        if (flag) {
+//            val item = menu.add(
+//                0,
+//                TOGGLE_FLASH, 0, "Flash Off"
+//            )
+//            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+//            flag = false
+//        } else {
+        if (scannerView.flash) {
             val item = menu.add(
                 0,
                 TOGGLE_FLASH, 0, "Flash Off"
             )
             item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-            flag = false
         } else {
-            if (scannerView.flash) {
-                val item = menu.add(
-                    0,
-                    TOGGLE_FLASH, 0, "Flash Off"
-                )
-                item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-            } else {
-                val item = menu.add(
-                    0,
-                    TOGGLE_FLASH, 0, "Flash On"
-                )
-                item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-            }
+            val item = menu.add(
+                0,
+                TOGGLE_FLASH, 0, "Flash On"
+            )
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
         }
+//        }
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -70,6 +69,8 @@ class BarcodeScannerActivity : Activity(), ZXingScannerView.ResultHandler {
 
     override fun onResume() {
         super.onResume()
+        scannerView.flash = true
+        invalidateOptionsMenu()
         scannerView.setResultHandler(this)
         // start camera immediately if permission is already given
         if (!requestCameraAccessIfNecessary()) {
